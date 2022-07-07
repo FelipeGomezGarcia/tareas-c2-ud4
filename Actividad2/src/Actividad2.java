c82e17f2a2150b3f2b837fd7c95c13b567fc2f5b
FelipeGomezGarcia <feli.goga@gmail.com>
2022-07-07 08:34:43 +0200

    Actividades 1 y 2

---
+++ Actividad2/src/Actividad2.java
@@ -0,0 +1,24 @@
+
+public class Actividad2 {
+	public static void main (String args[]) {
+		
+		int N = 7;
+		double A = 2.57;
+		char C = 'c';
+		
+		double resultado;
+		
+		System.out.println("Variable N = " + N);
+		System.out.println("Variable A = " + A);
+		System.out.println("Variable C = " + C);
+		
+		resultado = N + A;
+		System.out.println(N + " + " + A + " = " + resultado);
+		
+		resultado = A - N;
+		System.out.println(A + " - " + N + " = " + resultado);
+		
+		resultado = C;
+		System.out.println("El valor numerico  del caracter c = " + resultado);
+	}
+}
