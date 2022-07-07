e0502c740b6392443355710219a928b9cdc5c351
FelipeGomezGarcia <feli.goga@gmail.com>
2022-07-07 08:35:02 +0200

    Actividad 1

--- Actividad1/src/Actividad1.java
+++ Actividad1/src/Actividad1.java
@@ -3,15 +3,12 @@ public class Actividad1 {
 	public static void main (String args[]) {
 		
 		int num1 = 10 , num2 = 3;
-		double resultado;
-		
-		resultado = num1/num2;
 		
 		System.out.println(num1 + " + "+ num2 + " = " + (num1+num2));
 		System.out.println(num1 + " - "+ num2 + " = " + (num1-num2));
 		System.out.println(num2 + " - "+ num1 + " = " + (num2-num1));
 		System.out.println(num1 + " * "+ num2 + " = " + (num1*num2));
-		System.out.println(num1 + " : "+ num2 + " = " + resultado);
+		System.out.println(num1 + " : "+ num2 + " = " + (num1/num2));
 		System.out.println(num1 + " % "+ num2 + " = " + (num1%num2));
 	}
 }
